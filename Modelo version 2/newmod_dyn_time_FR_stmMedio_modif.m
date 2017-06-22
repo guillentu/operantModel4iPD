706d89c5950865976d59b17866b8399e7c34c2a1
robinriehl <robin.riehl@laposte.net>
2017-06-21 21:25:54 -0300

    summary
    d

--- Modelo version 2/newmod_dyn_time_FR_stmMedio_modif.m
+++ Modelo version 2/newmod_dyn_time_FR_stmMedio_modif.m
@@ -57,7 +57,7 @@ x_2=zeros(30,1);
 sesion = zeros(Nses,1);
 trialXsesion = zeros(Nses,Ntrial);
 terminal_sesion=zeros(Nses,1);
-nb_coop = zeros(Nses,Ntest);
+nb_resp = zeros(Nses,Ntest);
 contadorP1=0;
 contadorP2=0;
 
@@ -155,7 +155,7 @@ for l = 1:Ntest  %% TESTES
           contadorP1=0;
         end
       else
-        contador=contador +1;
+        contador++;%=contador +1;
         Rf_1=0;
         Rf_2=0;
       end
@@ -184,12 +184,13 @@ for l = 1:Ntest  %% TESTES
     Num = j+Num;
     Sizemat = length(A1);
     
-    for j = 1:Ntrial
+    for j = 1:length(palanca)
       if palanca(j)==1
         %nb_coop(k-1,l)= nb_coop(k-1,l)+1;
-        nb_coop(k,l)= nb_coop(k,l)+1;
+        nb_resp(k,l)= nb_resp(k,l)+1;
       end
     end
+    
   end
 end
 
@@ -251,11 +252,11 @@ end
 %%% PRUEBA V _ PORCENTAJE DE COOPERACION POR SESION / EXPERIMENTO
 
 
-porcentaje_coop_sesion=zeros(Nses,Ntest);
-porcentaje_coop_experimento=zeros(Ntest,1);
-for k = 1:Ntest
-  porcentaje_coop_experimento(k)=sum(nb_coop(:,k))/(Ntrial*Nses)*100;
-end
+%porcentaje_coop_sesion=zeros(Nses,Ntest);
+%porcentaje_coop_experimento=zeros(Ntest,1);
+%for k = 1:Ntest
+%  porcentaje_coop_experimento(k)=sum(nb_coop(:,k))/(Ntrial*Nses)*100;
+%end
 
 %for i=1:Ntest
 %  terminal_sesion(1,i)=sesion(1,i);
@@ -268,16 +269,16 @@ end
 
 
 %promedio_sesion=sum(ird)/Ntest
-porcent_10xsuperior_al_otra=porcent/Ntest * 100;
-cuanto_alcanzan_al_maxima=sum(maxima)/Ntest * 100;
-porcentaje_coop_experimento;
-porcentaje_coop_sesion=floor(nb_coop/Ntrial*100);
+%porcent_10xsuperior_al_otra=porcent/Ntest * 100;
+%cuanto_alcanzan_al_maxima=sum(maxima)/Ntest * 100;
+%porcentaje_coop_experimento;
+resp_por_segundos=nb_resp/length(palanca); %numero de respuestas dividido por el numero de respestas maximal posible
 
 
 for i=1:Ntest
   figure
   hold on
-  plot((1:Sizemat)*tMuestreo,A1(:,i),'r',(1:Sizemat)*tMuestreo,A2(:,i),'b',(1:Sizemat)*tMuestreo,stm_1(:,i),'g',(1:Sizemat)*tMuestreo,stm_2(:,i),'m',(1:Sizemat)*tMuestreo,limrand*ones(1,length([1:Sizemat])),'--b')
+  plot((1:Sizemat)*tMuestreo,A1(:,i),'r',(1:Sizemat)*tMuestreo,A2(:,i),'b',(1:Sizemat)*tMuestreo,stm_1(:,i),'g',(1:Sizemat)*tMuestreo,stm_2(:,i),'m',(1:Sizemat)*tMuestreo,limrand*ones(1,Sizemat),'--b')
   legend("A1","A2","STM1","STM2","limrand");
   for j=1:Nses
     %plot(sesion(j,i)*ones(1,max(max(A1(:,i)))),(1:max(max(A1(:,i)))),'--k');
@@ -299,14 +300,23 @@ color = 'rgbmkmrgbk';
 figure
 hold on
 for i=1:Ntest
-  plot(1:Nses,porcentaje_coop_sesion(:,i),'Color',color(i));
-  title('Porcentaje de Cooperacion');
-  xlabel('Numero de sesion');
-  ylabel('Porcentaje');
+  plot(Fr,resp_por_segundos(:,i),'Color',color(i));
+  title('Respuestas por segundos');
+  xlabel('Tasa fija');
+  ylabel('Respuestas/segundos');
 end
 hold off
 
-%%% probl�me de temps, pas le temps de d�croitre du coup red�marre plus
+figure
+hold on
+for i=1:Ntest
+  plot(Fr,resp_por_segundos(:,i),'Color',color(i));
+  title('Respuestas por segundos');
+  xlabel('Tasa fija');
+  ylabel('Respuestas/segundos');
+end
+hold off
+
 %%% haut CQFD
 %%%% CASTIGO DE TIEMPO 
 %%%% CUANTO VECES ELIGE COOPERATION
