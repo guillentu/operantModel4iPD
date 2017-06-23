701d71ae8012be4a3d621658bcae93d147f49825
robinriehl <robin.riehl@laposte.net>
2017-06-23 19:35:06 -0300

    VI

--- Modelo version 2/newmod_dyn_time_VI_stmMedio_modif.m
+++ Modelo version 2/newmod_dyn_time_VI_stmMedio_modif.m
@@ -58,7 +58,7 @@ d=1;
 sesion = zeros(Nses,1);
 trialXsesion = zeros(Nses,Ntrial);
 terminal_sesion=zeros(Nses,1);
-nb_coop = zeros(Nses,Ntest);
+nb_resp = zeros(Nses,Ntest);
 contadorP1=0;
 contadorP2=0;
 contadorI=0;
@@ -175,15 +175,15 @@ for l = 1:Ntest  %% TESTES
             contadorP2=0;
             contadorI =0;
             dispe=.5; %% dispercion del 50% del valor de la intervalo
-            intervalo=Vi(k)*(1 + dispe*(1-2*rand))
-            Vi(k)
+            intervalo=Vi(k)*(1 + dispe*(1-2*rand));
+            Vi(k);
           else
             contadorP1=0;
             contadorP2=0;
             contadorI =0;
             dispe=.5; %% dispercion del 50% del valor de la intervalo
-            intervalo=Vi(k)*(1 + dispe*(1-2*rand))
-            Vi(k)
+            intervalo=Vi(k)*(1 + dispe*(1-2*rand));
+            Vi(k);
           endif
         end
       else
@@ -217,10 +217,10 @@ for l = 1:Ntest  %% TESTES
     Num = j+Num;
     Sizemat = length(A1);
     
-    for j = 1:Ntrial
+    for j = 1:length(palanca)
       if palanca(j)==1
         %nb_coop(k-1,l)= nb_coop(k-1,l)+1;
-        nb_coop(k,l)= nb_coop(k,l)+1;
+        nb_resp(k,l)= nb_resp(k,l)+1;
       end
     end
   end
@@ -284,11 +284,11 @@ end
 %%% PRUEBA V _ PORCENTAJE DE COOPERACION POR SESION / EXPERIMENTO
 
 
-porcentaje_coop_sesion=zeros(Nses,Ntest);
-porcentaje_coop_experimento=zeros(Ntest,1);
-for k = 1:Ntest
-  porcentaje_coop_experimento(k)=sum(nb_coop(:,k))/(Ntrial*Nses)*100;
-end
+%porcentaje_coop_sesion=zeros(Nses,Ntest);
+%porcentaje_coop_experimento=zeros(Ntest,1);
+%for k = 1:Ntest
+%  porcentaje_coop_experimento(k)=sum(nb_coop(:,k))/(Ntrial*Nses)*100;
+%end
 
 %for i=1:Ntest
 %  terminal_sesion(1,i)=sesion(1,i);
@@ -301,10 +301,10 @@ end
 
 
 %promedio_sesion=sum(ird)/Ntest
-porcent_10xsuperior_al_otra=porcent/Ntest * 100;
-cuanto_alcanzan_al_maxima=sum(maxima)/Ntest * 100;
-porcentaje_coop_experimento;
-porcentaje_coop_sesion=floor(nb_coop/Ntrial*100);
+%porcent_10xsuperior_al_otra=porcent/Ntest * 100;
+%cuanto_alcanzan_al_maxima=sum(maxima)/Ntest * 100;
+%porcentaje_coop_experimento;
+%porcentaje_coop_sesion=floor(nb_coop/Ntrial*100);
 
 
 for i=1:Ntest
@@ -317,9 +317,9 @@ for i=1:Ntest
     h=plot((sesion(j,i).*tMuestreo)*ones(1,max(max(A1(:,i)))),(1:max(max(A1(:,i)))),'--k');
     set(h, "linewidth", 2);
     if j==1
-      h=plot(0:sesion(j,i)/4,P1(:,j)'*A1max,'.-m');
+      h=plot(1:sesion(j,i),P1(:,j)'*A1max,'.-m');
     else
-      h=plot(sesion(j-1,i)/4:sesion(j,i)/4,P1(:,j)'*A1max,'.-m');
+      h=plot(sesion(j-1,i)+1:sesion(j,i),P1(:,j)'*A1max,'.-m');
     endif
   endfor
   plot((0:sesion(Nses,i))/4,A1max*ones(1,length(0:sesion(Nses,i))))
@@ -327,6 +327,18 @@ for i=1:Ntest
 endfor
 
 %% Maximos exitos por intervalo= 10-360 - 30-120 - 40-65 - 60-60 
+
+color = 'rgbmkmrgbk';
+figure
+hold on
+for i=1:Ntest
+  plot(1:Nses,porcentaje_coop_sesion(:,i),'Color',color(i));
+  title('Porcentaje de Cooperacion');
+  xlabel('Numero de sesion');
+  ylabel('Porcentaje');
+end
+hold off
+
 
 color = 'rgbmkmrgbk';
 figure
