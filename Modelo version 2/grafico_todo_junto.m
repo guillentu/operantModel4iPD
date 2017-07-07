d725848590c690919ae2e5e26f8d445b35663b49
robinriehl <robin.riehl@laposte.net>
2017-07-07 20:30:18 -0300

    a

--- Modelo version 2/grafico_todo_junto.m
+++ Modelo version 2/grafico_todo_junto.m
@@ -8,9 +8,9 @@ Ntrial  =  1 ;   %% un trial de 3600 segundos
 tTrial  =  3600; %% en segundos
 tResp   =  1;    %% en segundos
 tMuestreo = 0.25;%% en segundos
-Nses    =  7 ;
+Nses    =  6 ;
 Ntest   =  1;
-
+N=[10,30,40,60,90,120];
 
 load FI
 load VI
@@ -39,7 +39,7 @@ for i=1:Nses
   hold on
   plot(1:Ntrial*tTrial,cum_respFI(i,:),'r',1:Ntrial*tTrial,cum_respFR(i,:),'g',1:length(cum_respVI(i,:)),cum_respVI(i,:),'b',1:length(cum_respVR(i,:)),cum_respVR(i,:),'c');
   plot(aFI.(num2str(i)),cum_respFI(i,aFI.(num2str(i))),'k+',aFR.(num2str(i)),cum_respFR(i,aFR.(num2str(i))),'k+',aVI.(num2str(i)),cum_respVI(i,aVI.(num2str(i))),'k+',aVR.(num2str(i)),cum_respVR(i,aVR.(num2str(i))),'k+');
-  title(strcat('Cumulative records sesion ',num2str(i),' todos juntos'));
+  title(strcat('Cumulative records N= ',num2str(N(i)),' todos juntos'));
   xlabel('Time');
   ylabel('Cumulative responses');
   legend("FI","FR","VI","VR");
